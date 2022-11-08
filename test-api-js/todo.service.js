d15d1db93b99e01f6160ce22c85ec4d373c172e6
meghanapothireddy <meghanareddy.pothireddy@slu.edu>
2022-11-07 22:07:52 -0600

    uploading todo functions

--- test-api-js/todo.service.js
+++ test-api-js/todo.service.js
@@ -5,11 +5,11 @@ class todoservice{
             "description": "D1",
             "done": false
         },{
-            "title": "T1",
+            "title": "T2",
             "description": "D1",
             "done": false
         },{
-            "title": "T1",
+            "title": "T3",
             "description": "D1",
             "done": false
         }]
@@ -24,14 +24,29 @@ class todoservice{
 
     add_todo(todo){
         // Your code here
+         this.todo_data.todo.push(todo) 
     }
 
     delete_todo(id){
         // Your code here
+        console.log("Before deleting : ",this.todo_data.todo);
+        this.todo_data.todo.splice(3, 1);
+        console.log("After deleting : ",this.todo_data.todo);
+        return this.todo_data.todo;
     }
 
     update_todo(id, todo){
         // Your code here
+        //todo={"title" : "T2", "description": "D2", "done":false}
+        //let index = 3;
+        //let ElementIndex = this.todos.todo.findIndex((todo) => todo.id == id);
+        console.log("Before update: ", this.todo_data.todo[id]);
+        this.todo_data.todo[id] = todo;
+        // this.todo_data.todo[id].description = todo.description;
+        // this.todo_data.todo[id].done = todo.done;
+        console.log("After update: ", this.todo_data.todo[id]);
+        return this.todo_data.todo[id];
+
     }
 }
 
